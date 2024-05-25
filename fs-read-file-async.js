a641e4d8d9b382828ccc92930cbaf406e68121a0
Grajaal <jorgegrajal@gmail.com>
2024-05-26 00:55:38 +0200

    file-system module

---
+++ fs-read-file-async.js
@@ -0,0 +1,16 @@
+// Import the fs module (node prefix for core modules)
+const fs = require('node:fs')
+
+// fs.readFile(...)  para leer el archivo de manera asíncrona. 
+// const text = fs.readFile('./file.txt')
+
+console.log("Leyendo el primer archivo")
+fs.readFile('./file.txt', 'utf-8', (err, text) => {
+    console.log(text)
+})
+
+console.log("Leyendo el segundo archivo")
+fs.readFile('./file2.txt', 'utf-8', (err, text) => {
+    console.log(text)
+})  
+
