82e8defeb5e02fed89dc2c8a81b1c280e8649f9d
Grajaal <jorgegrajal@gmail.com>
2024-05-26 14:16:25 +0200

    Global variable 'process'

---
+++ process.js
@@ -0,0 +1,16 @@
+// Argumentos de entrada
+console.log(process.argv)
+
+// Argumentos de salida 
+// process.exit(1)
+    
+// Controlar eventos del proceso
+process.on('exit', () => {
+    // Limpiar los recursos
+})
+
+// Current working directory: Desde que directorio hemos inicializado el proceso 
+console.log(process.cwd())
+
+// Platform
+console.log(process.env.PEPITO)
