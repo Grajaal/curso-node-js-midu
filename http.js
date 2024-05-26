066d456dede1514e72d582d4a1737ad8a4f6f42a
Grajaal <jorgegrajal@gmail.com>
2024-05-26 16:19:00 +0200

    free-port finder app

--- http.js
+++ http.js
@@ -1,10 +1,13 @@
 const http = require('node:http')
+const { findAvaiblePort } = require('./free-port.js')
 
 const server = http.createServer((req, res) => {
   console.log('Request received')
   res.end('Hi world!')
 })
 
-server.listen(0, () => {
-  console.log(`Server listenening on http://localhost:${server.address().port}`)
+findAvaiblePort(3306).then(port => {
+  server.listen(port, () => {
+    console.log(`Server listening on port http://localhost:${port}`)
+  })
 })
